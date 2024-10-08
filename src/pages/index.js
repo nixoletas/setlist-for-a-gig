b3111c69ace7e76293cdca023c9c2f96826e6048
Nicholas_Miyasato <nicholetas@gmail.com>
2024-10-07 23:15:45 -0400

    replace docs/comecando for docs/intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/comecando">
+            to="/docs/intro">
             Songs
           </Link>
         </div>
